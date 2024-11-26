bff00721456e4991e0312ae68cc7df12e4cc271f
Arch <soulyraziel@gmail.com>
2024-11-26 13:05:51 +0300

    Added new settings

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,12 +1,13 @@
 import scrapy
 
 from pep_parse.items import PepParseItem
+from pep_parse.settings import PEPS_DOMAIN, PEPS_URL
 
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    allowed_domains = [PEPS_DOMAIN]
+    start_urls = [PEPS_URL]
 
     def parse(self, response):
         peps = response.css('tbody tr td:nth-child(2) a::attr(href)').getall()
@@ -17,11 +18,11 @@ class PepSpider(scrapy.Spider):
         title = response.css('#pep-content h1::text').get().split(' – ')
         number = int(title[0].replace('PEP ', ''))
         name = ' – '.join(title[1:])
-        data = {
-            'number': number,
-            'name': name,
-            'status': response.css(
+        data = dict(
+            number=number,
+            name=name,
+            status=response.css(
                 'dt:contains("Status") + dd abbr::text'
             ).get()
-        }
+        )
         yield PepParseItem(data)
